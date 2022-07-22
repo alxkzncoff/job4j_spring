5864518d802f894981c5708ef5e2b583e96bf9b9
Aleksandr <alx.kzncoff@gmail.com>
2022-07-22 16:09:13 +0300

    2. Сканирование проекта [#1070]

--- src/main/java/ru/job4j/di/SpringDI.java
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -1,13 +1,11 @@
-package di;
+package ru.job4j.di;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SpringDI {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.register(Store.class);
-        context.register(ConsoleInput.class);
-        context.register(StartUI.class);
+        context.scan("ru.job4j.di");
         context.refresh();
 
         StartUI ui = context.getBean(StartUI.class);
